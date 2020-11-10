3cd448442e7cceb715e106cde1aa6b3f0e40636e
zachariah-hyman <zhyman0330@gmail.com>
2020-11-10 21:07:06 +0200

    formats camps and their locals into a csv

--- wikiScraper.py
+++ wikiScraper.py
@@ -1,33 +1,35 @@
 from selenium import webdriver
 import requests
 from bs4 import BeautifulSoup
+import csv
 
-# driver = webdriver.Chrome('/Users/zachariah/internship!/chromedriver')
-#
 url = "https://commons.wikimedia.org/wiki/Category:Nazi_concentration_camps_by_name"
-# driver.get(url)
 
-# links = driver.find_elements_by_xpath("//a[@href]")
 r = requests.get(url)
 soup = BeautifulSoup(r.content, 'lxml')
 links = soup.find_all('a')
 i = 0
-for link in links:
-	if i >= 10 and i < 20:
-		print(link.text)
-		url = "https://commons.wikimedia.org/%s"%(link.get('href'))
-		r = requests.get(url)
-		soup = BeautifulSoup(r.content, 'lxml')
-		# print(url)
-		info = soup.findAll('th')
-		for category in info:
-			if category.text == "Location":
-				locations = category.findNext('td')
-				for location in locations.findAll('a'):
-					print(location.text)
-				# next = (category.findNext('a').text)
+locationFound = False
+name = None
 
-		# print(location[1].findNext('td'))
-		# print(location[1].findAll('a'))
-		print("\n\n")
-	i += 1
+with open('spreadsheet.csv', 'w', newline='') as csvfile:
+	fieldnames = ['Camp', 'Location']
+	writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+	writer.writeheader()
+
+	for link in links:
+		if i >= 4 and i < 98:
+			url = "https://commons.wikimedia.org/%s"%(link.get('href'))
+			r = requests.get(url)
+			soup = BeautifulSoup(r.content, 'lxml')
+			info = soup.findAll('th')
+			for category in info:
+				if category.text == "Location":
+					locations = category.findNext('td')
+					for location in locations.findAll('a'):
+						name = location.text
+			if not name:
+				name = 'N/A'
+			writer.writerow({'Camp': link.text, 'Location': name})
+			name = None
+		i += 1
